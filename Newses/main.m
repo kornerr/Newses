c25c73d4827bb82c2f2460793d4a702ad80ad36d
Admin <admin@Mac-Admin.local>
2014-08-01 09:01:38 +0400

    Initial Commit

---
+++ Newses/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Newses
+//
+//  Created by Admin on 01.08.14.
+//  Copyright (c) 2014 VantuZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NWAppDelegate class]));
+    }
+}
---
+++ NewsesTests/NewsesTests.m
@@ -0,0 +1,34 @@
+//
+//  NewsesTests.m
+//  NewsesTests
+//
+//  Created by Admin on 01.08.14.
+//  Copyright (c) 2014 VantuZ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NewsesTests : XCTestCase
+
+@end
+
+@implementation NewsesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
