8dd87a01535637f3a1f7e5d3f1cb4ab857989cd8
Admin <admin@Mac-Admin.local>
2014-08-08 08:57:26 +0400

    add VKSdk

---
+++ NewsesProject/Newses/NWAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  NWAppDelegate.h
+//  Newses
+//
+//  Created by Admin on 01.08.14.
+//  Copyright (c) 2014 VantuZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "VKSdk.h"
+
+@interface NWAppDelegate : UIResponder <UIApplicationDelegate, VKSdkDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
